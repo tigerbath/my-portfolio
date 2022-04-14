5900aef863a5593cf4c686621026f60520cb485e
Matthew Palmer <mattjpalmer92@gmail.com>
2022-04-14 14:26:00 +0100

    import & set up

---
+++ src/AppRoutes.js
@@ -0,0 +1,9 @@
+import {} from "react-router-dom";
+
+import { About } from "./pages/About";
+import { Work } from "./pages/Work";
+import { Contact } from "./pages/Contact";
+
+export const AppRoutes = () => {
+    return ();
+};
