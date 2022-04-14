bcb3b77f0e46a08ba593eff4bd21e8bcbd15fb3f
Matthew Palmer <mattjpalmer92@gmail.com>
2022-04-14 14:30:41 +0100

    connect routes

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,9 +1,17 @@
-import {} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import { About } from "./pages/About";
 import { Work } from "./pages/Work";
 import { Contact } from "./pages/Contact";
 
 export const AppRoutes = () => {
-    return ();
+  return (
+    <Routes>
+      <Route path="/about" element={<About />}></Route>
+      <Route path="/work" element={<Work />}></Route>
+      <Route path="/contact" element={<Contact />}></Route>
+      // any other path route to about
+      <Route path="*" element={<About />}></Route>
+    </Routes>
+  );
 };
